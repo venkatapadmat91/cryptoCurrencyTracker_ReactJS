8835fce79a2218fc4ce442334d9c7e5bb17909d6
TallaPadma <venkatapadmat91@gmail.com>
2023-07-06 05:38:31 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,9 @@
+/* https://cryptotrker56.ccbp.tech */
+
+import CryptocurrencyTracker from './components/CryptocurrencyTracker'
+
+import './App.css'
+
+const App = () => <CryptocurrencyTracker />
+
+export default App
---
+++ src/components/CryptocurrencyItem/index.js
@@ -0,0 +1,30 @@
+import './index.css'
+
+const cryptocurrencyItem = props => {
+  const {cryptocurrencyDetails} = props
+  const {
+    currencyLogoUrl,
+    currencyName,
+    usdValue,
+    euroValue,
+  } = cryptocurrencyDetails
+
+  return (
+    <li className="cryptoCurrency-item">
+      <div className="logo-and-title-container" data-testid="loader">
+        <img
+          src={currencyLogoUrl}
+          alt={currencyName}
+          className="currency-logo"
+        />
+        <p className="currency-name">{currencyName}</p>
+      </div>
+      <div className="usd-and-euro-values-container">
+        <p className="currency-value">{usdValue}</p>
+        <p className="currency-value">{euroValue}</p>
+      </div>
+    </li>
+  )
+}
+
+export default cryptocurrencyItem
